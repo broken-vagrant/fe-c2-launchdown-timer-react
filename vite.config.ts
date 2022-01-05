63efa497c3778e50759ceedd3e0e5b74f77b06a0
zkindest <zkindest5@gmail.com>
2022-01-05 21:30:56 +0530

    add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fe-c4-launchdown-timer/',
+  base: '/fe-c2-launchdown-timer-react/',
 })
